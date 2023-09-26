ab84adfa58530ed747ff79bbfc756780abbc94d3
vlorian <vlorian.dervishi@gmail.com>
2023-09-26 15:26:38 -0400

    updated about paragraph

--- src/components/About/About-Me-index.js
+++ src/components/About/About-Me-index.js
@@ -9,17 +9,30 @@ function About() {
         <div>
           <img class="dbt" src={picture}></img>
         </div>
-        {/* <p id="abtmep">
+        <h1 id="name">Software Developer</h1>
+        <p id="abtmep">
           {" "}
-          Cracking dad jokes by day, coding and crashing computers by night -
-          welcome to the world of a proud dad and software maven! As a father to
-          an amazing child, I've mastered the art of patience (every parent's
-          secret weapon). Professionally, I'm a software engineer, though my
-          roots in maintenance engineering keep me humble and handy around the
-          house. An MTSU alum, I've woven a rich tapestry of binary codes,
-          late-night debugging, and proudly holding the "Father of the Year"
-          mug.#End.{" "}
-        </p> */}
+          I have always had a passion for software engineering and the
+          innovative solutions it can bring to people's lives. This passion led
+          me to develop a diverse skill set that I can easily apply to software
+          engineering. During my tenure as a maintenance engineer, I gained
+          valuable experience that I can transfer to software engineering. I
+          developed the ability to troubleshoot complex systems, collaborate
+          effectively in a team, and pay close attention to detail. These
+          capabilities are equally important in software engineering, where
+          identifying and resolving software issues, working in a team, and
+          ensuring precision in every aspect of a program are essential for
+          achieving success. Furthermore, I have gained experience in using
+          cutting-edge technologies such as Javascript, Node.js, and Express.js
+          to solve complex problems and create innovative solutions. I am also
+          proficient in HTML and CSS, which enables me to create visually
+          appealing and user-friendly interfaces. Additionally, I have
+          experience in using Python, a versatile programming language that can
+          be used for various purposes, such as data analysis and machine
+          learning. Overall, my love for software engineering and my diverse
+          skill set enable me to approach problems from various angles and
+          develop creative solutions that meet the needs of users.
+        </p>
       </div>
     </main>
   );
